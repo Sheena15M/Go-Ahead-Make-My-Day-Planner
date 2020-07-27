b405f78a45722d606850a734541764e17f3bc04a
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-07-27 11:18:18 -0400

    first commit

---
+++ planner.js
@@ -0,0 +1,9 @@
+//Times for Planner from moment.js
+const now = moment().format('MMMM Do YYYY,' h:mm:ss a
+  
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+}
