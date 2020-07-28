ce15d60a13646a3767cf0fbfb1a02f34a7831ffd
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-07-28 17:35:45 -0400

    changes to html css and js

--- planner.js
+++ planner.js
@@ -1,9 +1,71 @@
 //Times for Planner from moment.js
-const now = moment().format('MMMM Do YYYY,' h:mm:ss a
-  
-function getLocalStorage(key) {
-    let value = localStorage.getItem(key);
-    if (value) {
-        $(`#text${key}`).text(value);
+var appointText = "";
+var appointTime = "";
+var currentDate;
+var currentTime;
+var currentContainer;
+var tempArray = [];
+var storedAppointments;
+var returnedAppointments;
+
+
+
+$(window).on("load", function () {
+ currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
+$("#currentDay").append(currentDate);
+currentTime = moment().format("H");
+//Make and store appointments
+function renderAppointments() {
+    storedAppointments = JSON.parse(localStorage.getItem("appointments"));
+    if (storedAppointments !== null) {
+        for (i = 0; i < storedAppointments.length; i++) {
+                returnedAppointments = storedAppointments[i];
+                details = returnedAppointments.details;
+                timeIndex = returnedAppointments.time;
+                timeIndex = timeIndex.replace(":00", '');
+                if (details !== null) {
+                    $("#" + timeIndex).children('div').children('div').children('textarea').val(details);
+                }
+            }
+        }
     }
-}
+
+renderAppointments();
+//Color change for past appointments, current hour and future appointments
+for (i = 0; i <= 23; i++) {
+    CurrentContainer = i;
+        if (currentTime == i) {
+            $('#' + CurrentContainer).addClass("present");
+            $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("present");
+        }
+        else if (currentTime > i) {
+            $('#' + CurrentContainer).addClass("past");
+            $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("past");
+        }
+        else {
+            $('#' + CurrentContainer).addClass("future");
+            $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("future");
+        }
+    }
+})
+
+
+
+$(".saveBtn").click(function () {
+    appointText = $(this).parent('div').children('div').children('textarea').val();
+    appointTime = $(this).parent('div').parent().attr("id");
+    appointment = {
+        time: appointTime,
+        details: appointText
+    }
+    tempArray = JSON.parse(localStorage.getItem("appointments"));
+    if (tempArray === null) {
+        localStorage.setItem('appointments', JSON.stringify([{ time: appointTime, details: appointText }]));
+    }
+    else {
+        tempArray.push(appointment);
+        localStorage.setItem("appointments", JSON.stringify(tempArray));
+
+    }
+    $(this).parent('div').children('div').children('textarea').replaceWith($('<textarea>' + appointText.addClass("textarea") + '</textarea>'));
+})
